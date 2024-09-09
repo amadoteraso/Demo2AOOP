43555458905ef2b1787d4e5d03242658c6ba409e
amadoteraso <aminwarnner@gmail.com>
2024-09-09 09:08:16 +0800

    First Code commit

---
+++ FirstPersonalDetails.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document
+  
+    </title>
+</head>
+<body>
+    <?php 
+    $fname = "Warnner";
+    $lname = "Amin";
+   echo "Hello everyone, My name is $fname $lname"."<br>";
+   echo "Hello everyone, My name is ".$fname.$lname;
+   ?>
+</body>
+
+</html>
